fb10ff2f5ac11a06d14feb44d595a3ec40e1f8bd
hktysk <komikansougou@gmail.com>
2021-03-14 01:20:21 +0900

    feat(templates/pages): ログインページのテンプレートを作成

---
+++ pages/login.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <LoginTemplate :buttons="buttons" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import { Button as LoginButton } from '../types/login.type'
+import LoginTemplate from '../components/templates/Login.vue'
+
+@Component({
+  components: {
+    LoginTemplate,
+  },
+})
+export default class Login extends Vue {
+  get buttons(): LoginButton[] {
+    return [
+      {
+        icon: require('@/assets/img/sns-icons/google.png'),
+        text: 'Google',
+        onClick: () => {},
+      },
+      {
+        icon: require('@/assets/img/sns-icons/github.png'),
+        text: 'Github',
+        onClick: () => {},
+      },
+      {
+        icon: require('@/assets/img/sns-icons/twitter.png'),
+        text: 'Twitter',
+        onClick: () => {},
+      },
+    ]
+  }
+}
+</script>
